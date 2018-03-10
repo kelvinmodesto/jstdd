e759c157dce0199ef61361d51c7cd5a6764d74d9
Kelvin Modesto <km.modesto@gmail.com>
2018-03-09 23:42:41 -0300

    Finalizando capitulo de modulos

--- src/variaveis.js
+++
@@ -1,8 +0,0 @@
-// variaveis
-var a = 2000;
-let b = "exemplo";
-const c = {
-	nome: "Silas",
-	idade: 40
-}
-const d = 40;
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,36 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const nodeENV = process.env.NODE_ENV || 'production';
 
 module.exports = {
-	entry: {
-		filename: './src/app.js'
-	},
-	output: {
-		filename: './build.js'
-	},
-	module: {
-		rules: [{
-			test: /\.js?$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					presets: [
-						['env', 'es2015']
-					]
-				}
-			}
-		}]
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			},
-			output: {
-				comments: false
-			}
-		})
-	]
-}
+  devtool: 'source-map',
+  entry: {
+    filename: './src/app.js',
+  },
+  output: {
+    filename: './build.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['env', 'es2015'],
+          ],
+        },
+      },
+    }],
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      sourceMap: true,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeENV) },
+    }),
+  ],
+};
